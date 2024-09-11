5834d821531ac52666b7d2222ee8f9b48c8c799d
BUSTAMANTE2034 <bustamanteservincarloseduardo@gmail.com>
2024-09-11 13:07:48 -0600

    Este sera el archivo inicial del programa

---
+++ ST.py
@@ -0,0 +1,8 @@
+#SUMA MULTIPLICACIÓN
+
+a = int (input("Ingresa a: "))
+b = int (input("Ingresa b: "))
+z = input("¿Quieres hacer Suma o Multiplicación S/M?")
+
+match z:
+    #Completar los casos para suma y multiplicación
